1545b5fe4ac77e4d727c94295551bde68bd1b2b7
jason <aizhaoyu@163.com>
2015-11-02 12:06:19 +0800

    Initial Commit

---
+++ ContactInformation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ContactInformation
+//
+//  Created by jason on 15/11/2.
+//  Copyright (c) 2015年 JasonAi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ContactInformation/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ContactInformation
+//
+//  Created by jason on 15/11/2.
+//  Copyright (c) 2015年 JasonAi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
